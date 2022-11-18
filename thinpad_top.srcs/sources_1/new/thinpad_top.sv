baa67a7305fbbd8d9c3f36c36da17abdbe55d8e9
于沛楠 <2020010877@secoder.net>
2022-11-18 13:08:50 +0800

    200M clk test (5555)

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -130,8 +130,8 @@ module thinpad_top (
   logic sys_clk;
   logic sys_rst;
 
-  assign sys_clk = clk_10M;
-  assign sys_rst = reset_of_clk10M;
+  assign sys_clk = clk_200M;
+  assign sys_rst = reset_of_clk200M;
 
   // push_btn
   logic trigger;
@@ -407,7 +407,7 @@ module thinpad_top (
   // 串口控制器模块
   // NOTE: 如果修改系统时钟频率，也需要修改此处的时钟频率参数
   uart_controller #(
-      .CLK_FREQ(10_000_000),
+      .CLK_FREQ(200_000_000),
       .BAUD    (115200)
   ) uart_controller (
       .clk_i(sys_clk),
